1f134e1582c418a76d5ed8f165378a7c2c65e750
Cris-alt-V <cristianhernandesvalexis@gmail.com>
2025-09-02 13:05:32 -0300

    Add files via upload
    Ingreso de actividad Parcial computo 1

---
+++ Rutas.py
@@ -0,0 +1,6 @@
+class Ruta:
+    def __init__(self, nombre, tipo, costo, tiempo_estimado):
+        self.nombre = nombre
+        self.tipo = tipo  # "urbana" o "rural"
+        self.costo = costo
+        self.tiempo_estimado = tiempo_estimado
---
+++ Viajes.py
@@ -0,0 +1,6 @@
+from Rutas import Ruta
+
+class Viaje:
+    def __init__(self, ruta, dia):
+        self.ruta = ruta  # instancia de Ruta
+        self.dia = dia    # Ej. "lunes"
